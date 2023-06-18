7f9d2f883632d4ac9c14187a47eb3be73e3a11cf
eli-souto-r <eli.souto.r@gmail.com>
2023-06-18 14:09:57 +0200

    sliderr prueba3

--- js/caruselprueba.js
+++ js/caruselprueba.js
@@ -1,26 +1,52 @@
-const sliderContainer = document.querySelector('.slider');
-const track = document.querySelector('.slider-track');
+const sliderContainer = document.querySelector('.Slider-cont');
+const track = document.querySelector('.Slider-track');
 const slides = Array.from(track.children);
 const prevBtn = document.querySelector('.Slider-btn:nth-child(1)');
 const nextBtn = document.querySelector('.Slider-btn:nth-child(2)');
+let slideIndex = 0;
 
-let slideWidth = slides[0].getBoundingClientRect().width;
+// Función para ajustar el ancho de las imágenes
+const adjustSlideWidth = () => {
+  const slideWidth = sliderContainer.offsetWidth;
 
-// Ajustar el ancho de las imágenes al tamaño del contenedor
-slides.forEach((slide) => {
-  slide.style.width = `${slideWidth}px`;
-});
+  slides.forEach((slide) => {
+    slide.style.width = `${slideWidth}px`;
+  });
 
-// Posicionar las imágenes en línea
-track.style.width = `${slideWidth * slides.length}px`;
+  track.style.width = `${slideWidth * slides.length}px`;
+
+  moveSlide(0);
+};
 
 // Función para desplazar el slider
-const moveSlide = (direction) => {
-  const currentTranslate = track.style.transform ? parseInt(track.style.transform.match(/(-?\d+)/)[1]) : 0;
-  const moveAmount = direction * slideWidth;
-  track.style.transform = `translateX(${currentTranslate + moveAmount}px)`;
+const moveSlide = (index) => {
+  const slideWidth = sliderContainer.offsetWidth;
+
+  track.style.transform = `translateX(-${slideWidth * index}px)`;
+  slideIndex = index;
 };
 
 // Eventos de clic
-prevBtn.addEventListener('click', () => moveSlide(1));
-nextBtn.addEventListener('click', () => moveSlide(-1));
+prevBtn.addEventListener('click', () => {
+  const prevIndex = slideIndex === 0 ? slides.length - 1 : slideIndex - 1;
+  moveSlide(prevIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  const nextIndex = slideIndex === slides.length - 1 ? 0 : slideIndex + 1;
+  moveSlide(nextIndex);
+});
+
+// Funciones para avanzar y retroceder las imágenes
+function nextImage() {
+  const nextIndex = slideIndex === slides.length - 1 ? 0 : slideIndex + 1;
+  moveSlide(nextIndex);
+}
+
+function previousImage() {
+  const prevIndex = slideIndex === 0 ? slides.length - 1 : slideIndex - 1;
+  moveSlide(prevIndex);
+}
+
+// Inicialización y ajuste del ancho de las imágenes
+adjustSlideWidth();
