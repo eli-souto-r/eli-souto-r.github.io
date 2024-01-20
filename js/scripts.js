2e85d23cca91ff3089e7927cf4fa614475db832e
eli-souto-r <eli.souto.r@gmail.com>
2024-01-20 23:29:21 +0100

    2 intento

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,24 @@ cerrarButton.onclick = function(){
     contContacto.style.display="flex";
     
 }
+
+//Slider proyectos
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
+
+btbLeft.addEventListener("click", e => moveToLeft())
+btbRight.addEventListener("click", e => moveToRight())
+
+let operacion = 0;
+    widthIm = 100 / sliderSection.length;
+
+function moveToRight( ) {
+    operacion = operacion + widthIm
+    slider.style.transform = 'translate(-${operacion}%)';
+}
+
+function moveToLeft( ) {
+    
+}
